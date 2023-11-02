1c5005d267047b11d427bf8f97740d851c1e88ad
danilojoncic <djoncic14122rn@raf.rs>
2023-11-02 22:44:40 +0100

    dodavanje datuma za dogadjaje u rasporedu WIP

--- src/main/java/view/DateFrame.java
+++ src/main/java/view/DateFrame.java
@@ -2,6 +2,8 @@ package view;
 
 import com.toedter.calendar.JDateChooser;
 import controller.Cuvac;
+import model.boljeRijesenje.Datumi;
+import model.boljeRijesenje.Dogadjaj;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,6 +16,8 @@ public class DateFrame extends JFrame {
 
     MainFrame mainFrame;
     JButton krajniDatum;
+    JButton ucitajDatumeDugme;
+    private String[] dani = {"PON","UTO","SRI","CET","PET"};
 
     JDateChooser jDateChooser;
     JButton potvrdi;
@@ -32,7 +36,7 @@ public class DateFrame extends JFrame {
         this.setIconImage(icon.getImage());
         krajniDatum = new JButton("Kraj");
         this.setLayout(new FlowLayout());
-
+        ucitajDatumeDugme = new JButton("Ucitaj u model");
         jDateChooser = new JDateChooser();
         potvrdi = new JButton("Pocetak");
 
@@ -41,7 +45,7 @@ public class DateFrame extends JFrame {
         this.add(jDateChooser);
         this.add(potvrdi);
         this.add(krajniDatum);
-
+        this.add(ucitajDatumeDugme);
 
         krajniDatum.addActionListener(new ActionListener() {
             @Override
@@ -74,9 +78,50 @@ public class DateFrame extends JFrame {
             }
         });
 
+        ucitajDatumeDugme.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LocalDate localDate = null;
+                for(Dogadjaj dogadjaj : Cuvac.getInstance().getRaspored().getDogadjaji()){
+                    for(String s : dogadjaj.getStavkeDogadjaja()){
+                        for(int i = 0; i < dani.length;i++){
+                            if(s.equals(dani[i])){
+                                int index = Datumi.getInstance().vratiIndeksZaDan(s);
+                                localDate = getDatumZaDanUNedelji(index,Cuvac.getInstance().getRaspored().getPocetni());
+
+
+
+                            }
+                        }
+                    }
+                    dogadjaj.getStavkeDogadjaja().add(localDate.toString());
+                }
+                Cuvac.getInstance().getRaspored().refresh(Cuvac.getInstance().getRaspored().getDogadjaji());
+                for(Dogadjaj dogadjaj : Cuvac.getInstance().getRaspored().getDogadjaji()){
+                    System.out.println(dogadjaj);
+                }
+                //nadjem indeks dana na osnovu stringa iz stavke dogadjaja
+                //za zadati indeks zajedno sa pocetkom vazenje ja treba da nadjem datum
+                //taj datum kastujem u string koji dodajem u stavku dogadjdja kao novo polje
+            }
+        });
+
 
         this.setVisible(true);
 
     }
+    public LocalDate getDatumZaDanUNedelji(int indexDana, LocalDate pocetniDatum) {
+        if (indexDana < 0 || indexDana > 6) {
+            throw new IllegalArgumentException("Index dana u nedelji treba biti u opsegu od 0 do 6.");
+        }
+
+        // Pronalaženje razlike između traženog dana i dana početnog datuma
+        int razlika = (indexDana + 7 - pocetniDatum.getDayOfWeek().getValue()) % 7;
+
+        // Dodavanje razlike dana na početni datum kako bi se dobio traženi dan u nedelji
+        LocalDate trazeniDatum = pocetniDatum.plusDays(razlika);
+        return trazeniDatum;
+    }
+
 
 }
